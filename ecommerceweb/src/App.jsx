7e9342187d8a8f5a96d0a80010cf30caecd67694
PhuDangSaP <21522464@gm.uit.edu.vn>
2025-02-13 18:32:24 +0700

    push all

--- ecommerceweb/src/App.jsx
+++ ecommerceweb/src/App.jsx
@@ -1,7 +1,13 @@
-import React from 'react'
+import React,{useEffect} from 'react'
 import Layout from './components/Layout'
+import { useLocation } from "react-router-dom";
 
 export default function App() {
+  const { pathname } = useLocation();
+
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    }, [pathname]);
   return (
     <div><Layout/></div>
   )
